68ffc099de9e54fdd5cec728063307fd578fad71
jacklee5 <jack200336@gmail.com>
2023-02-19 00:52:23 -0500

    Update find-meal.js

--- diet-app/pages/find-meal.js
+++ diet-app/pages/find-meal.js
@@ -1,5 +1,6 @@
-import { Box, Button, Flex, Heading, SlideFade, Stack } from "@chakra-ui/react";
-import { useState } from "react";
+import { SearchIcon } from "@chakra-ui/icons";
+import { Alert, AlertDescription, AlertIcon, AlertTitle, Box, Button, Collapse, Flex, Heading, Input, InputGroup, InputLeftElement, SlideFade, Stack, Tag } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 const pageStyle = {
     position:"absolute", 
@@ -14,16 +15,23 @@ function Page({show, children}) {
     </SlideFade>
 }
 
-function CuisineOption({selected, children}) {
-    return <Button flexBasis="49%" bg={selected ? "primary.bg" : undefined}>
+function CuisineOption({selected, children, ...props}) {
+    return <Button 
+        flexBasis="49%" 
+        colorScheme={selected ? "red" : "gray"}
+        {...props}>
         {children}
     </Button>
 }
 
 export default function findMeal() {
-    const [page, setPage] = useState(1);
+    const [page, setPage] = useState(0);
     const [meal, setMeal] = useState();
-    const cuisines = ["Americanadsfasdf", "B", "C", "D", "E", "A", "B", "C", "D", "E"]
+    const [selectedCuisines, setSelectedCuisines] = useState([]);
+    const [error, setError] = useState("");
+
+    const cuisines = ['American', 'Asian', 'British', 'Caribbean', 'Central Europe', 'Chinese', 'Eastern Europe', 'French', 'Greek', 'Indian', 'Italian', 'Japanese', 'Korean', 'Kosher', 'Mediterranean', 'Mexican', 'Middle Eastern', 'Nordic', 'South American', 'Southeast Asian']
+    const cuisineVals = ['american', 'asian', 'british', 'caribbean', 'centraleurope', 'chinese', 'easterneurope', 'french', 'greek', 'indian', 'italian', 'japanese', 'korean', 'kosher', 'mediterranean', 'mexican', 'middleeastern', 'nordic', 'southamerican', 'southeastasian', 'world']
 
     const nextPage = () => setPage(page + 1);
 
@@ -32,6 +40,24 @@ export default function findMeal() {
             nextPage();
             setMeal(type);
         }
+    }
+
+    function toggleCuisine(i) {
+        const copy = [...selectedCuisines];
+        copy[i] = !copy[i];
+        console.log(copy);
+        setSelectedCuisines(copy);
+    }
+
+    function selectSurprise(i) {
+        nextPage();
+        setSelectedCuisines(cuisines.map(x => true));
+    }
+
+    function chooseCuisines() {
+        for (let el of selectedCuisines) 
+            if (el) return nextPage();
+        setError("Please choose at least one cuisine.")
     }
 
     return <Box pos="relative">
@@ -47,15 +73,29 @@ export default function findMeal() {
         </Flex>
     </Page>
     <Page show={page == 1}>
-        <Flex flexDir="column" height="100%" gap={8}>
+        <Flex flexDir="column" height="100%" gap={8} overflow="auto">
             <Heading>What kinds of cuisine do you want?</Heading>
-            <Button>✨ Surprise Me ✨</Button>
+            <Box my={-4}>
+            <Collapse in={error}>
+                <Alert status="error" mt={2}>
+                    <AlertIcon />
+                    <AlertDescription>{error}</AlertDescription>
+                </Alert>
+            </Collapse>
+            </Box>
+             
+            <Button onClick={selectSurprise} flexShrink={0}>✨ Surprise Me ✨</Button>
             <hr/>
-            <Flex flexDir="row" gap="2%" rowGap={2} flexWrap="wrap" flex={1} alignContent="flex-start">
+            <Flex flexDir="row" gap="2%" rowGap={2} flexWrap="wrap" alignContent="flex-start">
                 { 
-                    cuisines.map(cuisine => <CuisineOption>{cuisine}</CuisineOption>) 
+                    cuisines.map((cuisine, i) => <CuisineOption 
+                        selected={selectedCuisines[i]} 
+                        onClick={() => toggleCuisine(i)}>
+                            {cuisine}
+                        </CuisineOption>) 
                 }
             </Flex>
+            <Button bg="primary.bg" color="primary.fg" onClick={chooseCuisines} flexShrink={0}>Continue</Button>
         </Flex>
     </Page>
     </Box>
